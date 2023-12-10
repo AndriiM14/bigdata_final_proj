f8ee48e4ab252216bb9c2617c8733e2409ace1dc
BogdanaGonserovska <81592867+BogdanaGonserovska@users.noreply.github.com>
2023-12-10 23:44:42 +0200

    added 6 queries

--- columns.py
+++ columns.py
@@ -6,6 +6,11 @@ birth_year = "birth_year"
 death_year = "death_year"
 primary_profession = "primary_profession"
 known_for_titles = "known_for_titles"
+known_title = "known_title"
+title_count = "title_count"
+director_name = "director_name"
+writer_name = "writer_name"
+profession = "profession"
 
 # Titles akas dataframe columns
 
@@ -17,8 +22,9 @@ language = "language"
 types = "types"
 attributes = "attributes"
 is_original_title = "is_original_title"
+language_count = "language_count"
 
-# Tit;es id column
+# Titles id column
 
 tconst = "tconst"
 
@@ -32,17 +38,21 @@ start_year = "start_year"
 end_year = "end_year"
 runtime_minutes = "runtime_minutes"
 genres = "genres"
+genre = "genre"
 
 # Titles crew dataframe columns
 
 directors = "directors"
 writers = "writers"
+director = "director"
+writer = "writer"
 
 # Titles episode dataframe columns
 
 parent_tconst = "parent_tconst"
 season_number = "season_number"
 episode_number = "episode_number"
+max_episodes = "max_episodes"
 
 # Titles principals dataframe columns
 
