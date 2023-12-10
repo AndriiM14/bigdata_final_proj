dfc69112cbeda313d3094497345ecf9a235868f6
Andrii M <andrii.marusyk.knm.2020@lpnu.ua>
2023-12-10 12:40:31 +0200

    added settings file

--- dataset.py
+++ dataset.py
@@ -1,5 +1,4 @@
-from pyspark.sql import DataFrame as df
-from pyspark.sql import SparkSession
+from pyspark.sql import DataFrame as df, SparkSession
 from pyspark.sql.functions import split
 from pyspark.sql.types import ArrayType, StringType
 from dataclasses import dataclass
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
+#!/usr/bin/env python
 from pyspark.sql import SparkSession
 from dataset import load_dataset, preprocess
-
+from settings import DATA_PATH
 
 
 def main():
     spark = SparkSession.builder.appName("IMDBAnalysis").getOrCreate()
 
-    dataset = load_dataset("./data", spark)
+    dataset = load_dataset(DATA_PATH, spark)
     dataset = preprocess(dataset)
 
     dataset.nbasics.printSchema()
