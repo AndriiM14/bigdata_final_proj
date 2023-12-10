8762adab70bb7d1e3c29993f537b0e9442983c2d
Andrii Shchur <andriis@profisealabs.com>
2023-12-11 00:01:10 +0200

    first business question

--- jobs.py
+++ jobs.py
@@ -1,3 +1,6 @@
+from pyspark.sql.functions import col, avg, explode, count
+from pyspark.sql.functions import max as max_
+
 from dataset import Dataset
 from pyspark.sql import DataFrame as df
 import columns as c
@@ -7,3 +10,24 @@ def example_job(d: Dataset) -> None:
     d.tratings.groupBy(c.average_rating).count().show()
 
 
+def best_popular_directors_by_avg_movie_rating(d: Dataset):
+    title_ratings, title_crew, name_basics = d.tratings, d.tcrew, d.nbasics
+    joined_df = title_crew.select(
+        "tconst", explode("directors").alias("director")
+    ).join(title_ratings, "tconst")
+    name_basics_df = name_basics.select("nconst", "primary_name")
+    data = joined_df.join(name_basics_df, col("director") == col("nconst"), "left")
+    data = (
+        data.select("average_rating", "num_votes", "primary_name")
+        .groupBy("primary_name")
+        .agg(
+            avg("average_rating").alias("average_rating"),
+            max_("num_votes").alias("max_votes"),
+            count("*").alias("title_count"),
+        )
+        .filter(col("max_votes") > 50000)
+        .filter(col("title_count") > 5)
+        .orderBy("average_rating", ascending=False)
+    )
+
+    data.show()
--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 from pyspark.sql import SparkSession
 from dataset import load_dataset, preprocess
 from settings import DATA_PATH, OUTPUT_PATH
-from jobs import example_job
+from jobs import example_job, best_popular_directors_by_avg_movie_rating
+
 
 def main():
     spark = SparkSession.builder.appName("IMDBAnalysis").getOrCreate()
@@ -11,9 +12,9 @@ def main():
     dataset = preprocess(dataset)
 
     example_job(dataset)
+    best_popular_directors_by_avg_movie_rating(dataset)
 
     input("Press any button to end the program")
-
 
 
 if __name__ == "__main__":
